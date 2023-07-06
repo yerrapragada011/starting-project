8565db91e1d77e53a2e8ec72246b326678b1f02a
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-07-06 13:45:56 -0400

    Passing Data to Components with Props

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,8 @@
-const names = ['Maximilian', 'Manuel'];
-
-function Post() {
-  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-
+function Post(props) {
   return (
     <div>
-      <p>{chosenName}</p>
+      <p>{props.author}</p>
+      <p>{props.body}</p>
     </div>
   );
 }
