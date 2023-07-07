82d62432214540fbf26f347c576f468e64da4e28
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-07-07 12:53:27 -0400

    Preparing the App for State Management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import Post from './Post';
+import NewPost from './NewPost';
 import classes from './PostsList.module.css';
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author='Maximilian' body='React.js is awesome!' />
-      <Post author='Manuel' body='Check out the full course!' />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author='Maximilian' body='React.js is awesome!' />
+        <Post author='Manuel' body='Check out the full course!' />
+      </ul>
+    </>
   );
 }
 
