d081a7bb605c3ea237efbd0d2a1aa35004a079d2
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-07-10 14:53:41 -0400

    Sending a POST HTTP Request

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -9,6 +9,13 @@ function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
 
   function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
