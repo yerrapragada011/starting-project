032018130fdc36cd8de2afc28999aa407250f4cd
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-07-11 13:30:32 -0400

    Working with Layout Routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,17 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import NewPost from './components/NewPost';
 import './index.css';
+import RootLayout from './routes/RootLayout';
 
 const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/create-post', element: <NewPost /> },
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <App /> },
+      { path: '/create-post', element: <NewPost /> },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
